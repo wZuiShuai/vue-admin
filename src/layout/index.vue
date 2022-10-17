65f664a8b54b366c01ddc2a54c655df7ea28615b
LYC <1490579984@qq.com>
2022-10-17 22:09:46 +0800

    login

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,12 +1,17 @@
 <template>
-    header
-    <router-view></router-view>
-    floor
+  <el-container>
+    <el-aside>Aside</el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
-<script lang="ts" setup>
-</script>
+<script lang="ts" setup></script>
 
 <style lang="scss" scoped>
-
+.el-container {
+  height: 100%;
+}
 </style>
--- src/views/user/login.vue
+++ src/views/user/login.vue
@@ -1,14 +1,200 @@
 <template>
-    <div>
-{{state.token}}----{{state.getUserMsg}}
+  <div class="login f-center">
+    <div class="content">
+      <div class="left">
+        <img src="@/static/user/login_left0.png" alt="login" />
+      </div>
+      <div class="right">
+        <img
+          class="form_icon"
+          src="@/static/user/form_icon.png"
+          alt="form_icon"
+        />
+        <div class="box">
+          <div class="logo f-center">
+            <img src="@/assets/vue.svg" alt="logo" />
+            <p class="text">Admin</p>
+          </div>
+          <el-form ref="formRef" :model="user">
+            <el-form-item
+              prop="username"
+              :rules="[
+                {
+                  required: true,
+                  message: '请输入用户名',
+                  trigger: 'blur',
+                },
+              ]"
+            >
+              <el-input
+                v-model="user.username"
+                size="large"
+                placeholder="用户名："
+              >
+                <template #prefix>
+                  <el-icon>
+                    <User />
+                  </el-icon>
+                </template>
+              </el-input>
+            </el-form-item>
+            <el-form-item
+              prop="password"
+              :rules="{
+                required: true,
+                message: '请输入密码',
+                trigger: ['blur', 'change'],
+              }"
+            >
+              <el-input
+                v-model="user.password"
+                size="large"
+                show-password
+                autocomplete="new-password"
+                placeholder="密码："
+              >
+                <template #prefix>
+                  <el-icon>
+                    <lock />
+                  </el-icon>
+                </template>
+              </el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button :icon="CircleClose" size="large" round>重置</el-button>
+              <el-button type="primary" size="large" round>登录</el-button>
+            </el-form-item>
+          </el-form>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import {userStore} from '@/store/user'
-const state = userStore()
+import { User, Lock, CircleClose } from "@element-plus/icons-vue";
+import { reactive, ref } from "vue";
+import type { FormInstance } from "element-plus";
+
+import { ElLoading } from "element-plus";
+
+const fullscreenLoading = ref(false);
+const openFullScreen1 = () => {
+  fullscreenLoading.value = true;
+  setTimeout(() => {
+    fullscreenLoading.value = false;
+  }, 2000);
+};
+
+const openFullScreen2 = () => {
+  const loading = ElLoading.service({
+    lock: true,
+    text: "Loading",
+    background: "rgba(0, 0, 0, 0.7)",
+  });
+  setTimeout(() => {
+    loading.close();
+  }, 2000);
+};
+
+const formRef = ref<FormInstance>();
+
+const user = reactive<{
+  username: string;
+  password: string;
+}>({
+  username: "",
+  password: "",
+});
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.login {
+  height: 100%;
+  background: #eee url(@/static/user/login_bg.svg) no-repeat;
+  background-size: cover;
+  background-position: 50%;
+  min-height: 550px;
+  min-width: 550px;
 
+  .content {
+    width: 96%;
+    height: 94%;
+    padding: 0 4%;
+    background-color: hsla(0deg, 0%, 100%, 0.8);
+    border-radius: 10px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    .left {
+      max-width: 800px;
+
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+
+    .right {
+      flex-shrink: 0;
+      position: relative;
+      height: 395px;
+      width: 500px;
+
+      .form_icon {
+        position: absolute;
+        top: -55px;
+        right: -75px;
+      }
+
+      .box {
+        height: 100%;
+        width: 100%;
+        background-color: rgba(255, 255, 255, 0.98);
+        border-radius: 10px;
+        box-shadow: 2px 3px 7px rgb(0 0 0 / 20%);
+        position: absolute;
+        z-index: 2;
+        padding: 50px 40px 45px;
+        .logo {
+          margin-bottom: 45px;
+          img {
+            width: 60px;
+            height: 52px;
+          }
+
+          .text {
+            padding-left: 25px;
+            font-size: 2.334rem;
+            font-weight: bold;
+            color: #34495e;
+            white-space: nowrap;
+          }
+        }
+        .el-form {
+          .el-form-item {
+            margin-bottom: 40px;
+            .el-button {
+              flex: 1;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .login {
+    min-width: 100%;
+    .content {
+      overflow: hidden;
+      .right {
+        flex-shrink: 0;
+        width: 100%;
+      }
+    }
+  }
+}
 </style>
